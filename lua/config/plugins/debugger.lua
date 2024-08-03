a38822499a8cf822c001542c47e637bbd7168632
yuzhegan <boflyyang@163.com>
2024-08-03 08:05:23 +0800

    add python debuger

--- lua/config/plugins/debugger.lua
+++ lua/config/plugins/debugger.lua
@@ -77,6 +77,29 @@ return {
 					args = { "--port", "${port}" },
 				}
 			}
+			dap.adapters.python = {
+				type = 'executable',
+				command = 'python',
+				args = { '-m', 'debugpy.adapter' },
+			}
+			dap.configurations.python = {
+				{
+					type = 'python',
+					request = 'launch',
+					name = 'Launch file',
+					program = '${file}',
+				},
+			}
+			dap.adapters.cpp = {
+				type = 'executable',
+				name = 'cppdbg',
+				command = 'lldb-vscode',
+				args = {},
+				attach = {
+					pidProperty = 'processId',
+					pidSelect = 'ask',
+				},
+			}
 			dap.configurations.cpp = {
 				{
 					name = "Launch file",
